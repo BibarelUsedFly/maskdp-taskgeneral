e1f2bcf40d7a02176c0d57402235538662aba511
BibarelUsedFly <bibarelusedfly@gmail.com>
2025-09-17 18:58:34 -0300

    Minimal stuff needed to get to the logger instantiation

---
+++ pretest.py
@@ -0,0 +1,195 @@
+import warnings
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+import os
+
+os.environ["MKL_SERVICE_FORCE_INTEL"] = "1"
+os.environ["MUJOCO_GL"] = "disable" 
+
+from pathlib import Path
+
+import hydra
+# import numpy as np
+import torch
+# from dm_env import specs
+
+import dmc
+import utils
+from logger import Logger
+# from replay_buffer import make_replay_loader
+# from video import VideoRecorder
+import wandb
+import omegaconf
+from pprint import pprint
+
+torch.backends.cudnn.benchmark = True
+
+
+def get_dir(cfg):
+    resume_dir = Path(cfg.resume_dir)
+    snapshot = resume_dir / str(cfg.seed) / f"snapshot_{cfg.resume_step}.pt"
+    print("loading from", snapshot)
+    return snapshot
+
+
+def get_domain(task):
+    if task.startswith("point_mass_maze"):
+        return "point_mass_maze"
+    return task.split("_", 1)[0]
+
+
+def get_data_seed(seed, num_data_seeds):
+    return (seed - 1) % num_data_seeds + 1
+
+@hydra.main(config_path=".", config_name="pretrain")
+def main(cfg):
+    work_dir = Path.cwd()
+     # Print the actual directory determined by hydra config
+    print(f"Workspace: {work_dir}")
+
+    # Set seed for random, numpy and torch
+    utils.set_seed_everywhere(cfg.seed)
+    device = torch.device(cfg.device)
+    print(f"Using device: {device}")
+
+    # Create DeepMindControl environment for specified task
+    env = dmc.make(cfg.task, seed=cfg.seed)
+
+    # Create agent. Utils will instantiate a class
+    # Since cfg.agent is 'mdp'. The class will be specified by the mdp.yaml
+    # in the agent folder
+    agent = hydra.utils.instantiate(
+        cfg.agent,
+        obs_shape=env.observation_spec().shape,
+        action_shape=env.action_spec().shape,
+    )
+
+    # Create a snapshot directory for the task
+    domain = get_domain(cfg.task)
+    snapshot_dir = work_dir / Path(cfg.snapshot_dir) / domain / str(cfg.seed)
+    snapshot_dir.mkdir(exist_ok=True, parents=True)
+
+    # Create logger
+    cfg.agent.obs_shape = env.observation_spec().shape
+    cfg.agent.action_shape = env.action_spec().shape
+    exp_name = "_".join([cfg.agent.name, domain, str(cfg.seed)])
+    # Create wandb_config from Hydra's omegaconf
+    wandb_config = omegaconf.OmegaConf.to_container(
+        cfg, resolve=True, throw_on_missing=True
+    )
+    print(type(wandb_config))
+    pprint(wandb_config)
+    wandb.init(
+        project=cfg.project,
+        # This has to be your WandB user-institution
+        entity="bibarelusedfly-cenia", 
+        name=exp_name,
+        config=wandb_config,
+        settings=wandb.Settings(_disable_stats=True,),
+        mode="online" if cfg.use_wandb else "offline",
+        notes=cfg.notes,
+    )
+    logger = Logger(work_dir, use_tb=cfg.use_tb, use_wandb=cfg.use_wandb)
+
+# @hydra.main(config_path=".", config_name="pretrain")
+# def main(cfg):
+#     work_dir = Path.cwd()
+#     print(f"workspace: {work_dir}")
+
+#     # Set seed for random, numpy and torch
+#     utils.set_seed_everywhere(cfg.seed)
+#     device = torch.device(cfg.device)
+
+#     # create envs
+#     env = dmc.make(cfg.task, seed=cfg.seed)
+
+#     # create agent
+#     agent = hydra.utils.instantiate(
+#         cfg.agent,
+#         obs_shape=env.observation_spec().shape,
+#         action_shape=env.action_spec().shape,
+#     )
+
+#     if cfg.resume is True:
+#         resume_dir = get_dir(cfg)
+#         payload = torch.load(resume_dir)
+#         agent.model.load_state_dict(payload["model"])
+
+#     domain = get_domain(cfg.task)
+#     snapshot_dir = work_dir / Path(cfg.snapshot_dir) / domain / str(cfg.seed)
+#     snapshot_dir.mkdir(exist_ok=True, parents=True)
+
+#     # create logger
+#     cfg.agent.obs_shape = env.observation_spec().shape
+#     cfg.agent.action_shape = env.action_spec().shape
+#     exp_name = "_".join([cfg.agent.name, domain, str(cfg.seed)])
+#     wandb_config = omegaconf.OmegaConf.to_container(
+#         cfg, resolve=True, throw_on_missing=True
+#     )
+#     wandb.init(
+#         project=cfg.project,
+#         entity="Bibarel-cenia",
+#         name=exp_name,
+#         config=wandb_config,
+#         settings=wandb.Settings(
+#             start_method="thread",
+#             _disable_stats=True,
+#         ),
+#         mode="online" if cfg.use_wandb else "offline",
+#         notes=cfg.notes,
+#     )
+#     logger = Logger(work_dir, use_tb=cfg.use_tb, use_wandb=cfg.use_wandb)
+
+#     # This should be the path to the dataset for offline pretraining
+#     replay_train_dir = Path(cfg.replay_buffer_dir) / domain
+#     print(f"replay dir: {replay_train_dir}")
+#     train_loader = make_replay_loader(
+#         env,
+#         replay_train_dir,
+#         cfg.replay_buffer_size,
+#         cfg.batch_size,
+#         cfg.replay_buffer_num_workers,
+#         cfg.discount,
+#         domain,
+#         cfg.agent.transformer_cfg.traj_length,
+#         relabel=False,
+#     )
+#     train_iter = iter(train_loader)
+#     # create video recorders
+
+#     timer = utils.Timer()
+
+#     global_step = cfg.resume_step
+
+#     train_until_step = utils.Until(cfg.num_grad_steps)
+#     eval_every_step = utils.Every(cfg.eval_every_steps)
+#     log_every_step = utils.Every(cfg.log_every_steps)
+#   
+#   # True until global_step gets to cfg.num_grad_steps
+#     while train_until_step(global_step):
+#         # try to evaluate
+#         # Train on a single batch and permform a gradient step
+#         metrics = agent.update(train_iter, global_step)
+#         logger.log_metrics(metrics, global_step, ty="train")
+#         if log_every_step(global_step):
+#             elapsed_time, total_time = timer.reset()
+#             with logger.log_and_dump_ctx(global_step, ty="train") as log:
+#                 log("fps", cfg.log_every_steps / elapsed_time)
+#                 log("total_time", total_time)
+#                 log("step", global_step)
+
+#         if global_step in cfg.snapshots:
+#             snapshot = snapshot_dir / f"snapshot_{global_step}.pt"
+#             payload = {
+#                 "model": agent.model.state_dict(),
+#                 "cfg": cfg.agent.transformer_cfg,
+#             }
+#             with snapshot.open("wb") as f:
+#                 torch.save(payload, f)
+
+#         global_step += 1
+
+
+if __name__ == "__main__":
+    main()
